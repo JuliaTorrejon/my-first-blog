1f2bc19b9cd13568bb47455ca38afb5080f1e0ac
Julia <juliaesther.torrejon@gmail.com>
2019-01-23 22:18:08 +0000

    sign up

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
-
 from django.contrib.auth import views
+from django.conf.urls import url
+from mysite.core import views as core_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('blog.urls')),
+    url(r'^signup/$', core_views.signup, name='signup'),
    ]
